36aff2b3296ee860073f9618c2b5d4dca76c9f78
vadimor <vadimorbiluy@gmail.com>
2022-01-20 21:27:47 +0200

    Add configuration

---
+++ Module4HW3/Configuration/ProjectConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW3.Entity;
+
+namespace Module4HW3.Configuration
+{
+    public class ProjectConfiguration : IEntityTypeConfiguration<ProjectEntity>
+    {
+        public void Configure(EntityTypeBuilder<ProjectEntity> builder)
+        {
+            builder.ToTable("Project").HasKey(p => p.ProjectId);
+            builder.Property(p => p.ProjectId).ValueGeneratedOnAdd();
+            builder.Property(p => p.Name).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.Budget).HasColumnType("money").IsRequired();
+            builder.Property(p => p.StartedDate).HasMaxLength(7).IsRequired();
+        }
+    }
+}
