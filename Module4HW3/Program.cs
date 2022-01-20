e8cd0c1c49d6e7dcb445370fc99fae13558b72fb
vadimor <vadimorbiluy@gmail.com>
2022-01-20 17:20:53 +0200

    Добавьте файлы проекта.

---
+++ Module4HW3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module4HW3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
